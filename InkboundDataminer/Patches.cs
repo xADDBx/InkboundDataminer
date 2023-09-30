73d4d8c072142c51a1865c659f45f68ebca7494b
ADDB <addb1234567890@gmail.com>
2023-09-30 23:06:36 +0200

    Created ModEnabler

--- InkboundDataminer/Patches.cs
+++ InkboundDataminer/Patches.cs
@@ -1,91 +1,48 @@
 ﻿using HarmonyLib;
 using Mono.Btls;
 using Mono.Net.Security;
-using Mono.Security.Interface;
 using Mono.Unity;
-using NativeWebSocket;
-using ShinyShoe;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Reflection;
-using System.Threading;
 
-namespace InkboundDataminer {
+namespace InkboundModEnabler {
     public static class Patches {
-        public static Harmony HarmonyInstance => new Harmony("ADDB.Test");
-        /*
-        [HarmonyPatch(typeof(StatCategoryVisualData))]
-        public static class StatCategoryVisualData_Patch {
-            [HarmonyPatch(nameof(StatCategoryVisualData.GetStatCategories))]
-            [HarmonyPrefix]
-            public static bool GetStatCategories(StatCategoryVisualData __instance, ref IReadOnlyList<StatCategoryEntryData> __result) {
-                foreach (var stat in __instance.statCategories) {
-                    stat.hideEntriesIfZero = false;
-                    stat.onlyShowInHud = false;
-                }
-                __result = __instance.statCategories;
-                return false;
-            }
-        }
-        */
+        public static Harmony HarmonyInstance => new Harmony("ADDB.InkboundModEnabler");
         [HarmonyPatch(typeof(Mono.Net.Security.MonoTlsProviderFactory))]
         public static class MonoTlsProviderFactory_Patch {
             [HarmonyPatch(nameof(Mono.Net.Security.MonoTlsProviderFactory.CreateDefaultProviderImpl))]
             [HarmonyPrefix]
             public static bool CreateDefaultProviderImpl(ref MobileTlsProvider __result) {
-                var file = new StreamWriter("MyLogWTF.txt");
-                file.AutoFlush = true;
-                file.WriteLine("Start!");
-                try {
-                    string text = Environment.GetEnvironmentVariable("MONO_TLS_PROVIDER");
-                    if (string.IsNullOrEmpty(text)) {
-                        text = "default";
-                    }
-                    if (!(text == "default") && !(text == "legacy")) {
-                        if (!(text == "btls")) {
-                            if (!(text == "unitytls")) {
-                                __result = Mono.Net.Security.MonoTlsProviderFactory.LookupProvider(text, true);
-                                file.WriteLine("Done");
-                                file.Close();
-                                return false;
-                            }
-                            goto IL_6E;
-                        }
-                    } else {
-                        if (UnityTls.IsSupported) {
-                            goto IL_6E;
-                        }
-                        if (!Mono.Net.Security.MonoTlsProviderFactory.IsBtlsSupported()) {
-                            throw new NotSupportedException("TLS Support not available.");
-                        }
-                    }
-                    __result = new MonoBtlsProvider();
-                    file.WriteLine("Done");
-                    file.Close();
-                    return false;
-IL_6E:
-                    __result = new UnityTlsProvider();
-                    file.WriteLine("Done");
-                    file.Close();
-                    return false;
-                } catch (Exception ex) {
-                    file.WriteLine(ex.ToString());
+                string text = Environment.GetEnvironmentVariable("MONO_TLS_PROVIDER");
+                if (string.IsNullOrEmpty(text)) {
+                    text = "default";
                 }
-                file.WriteLine("Done");
-                file.Close();
+                if (!(text == "default") && !(text == "legacy")) {
+                    if (!(text == "btls")) {
+                        if (!(text == "unitytls")) {
+                            __result = Mono.Net.Security.MonoTlsProviderFactory.LookupProvider(text, true);
+                            return false;
+                        }
+                        goto IL_6E;
+                    }
+                } else {
+                    if (UnityTls.IsSupported) {
+                        goto IL_6E;
+                    }
+                    if (!Mono.Net.Security.MonoTlsProviderFactory.IsBtlsSupported()) {
+                        throw new NotSupportedException("TLS Support not available.");
+                    }
+                }
+                __result = new MonoBtlsProvider();
+                return false;
+IL_6E:
+                __result = new UnityTlsProvider();
                 return false;
             }
         }
 
         public static void Patch() {
-            try {
-                HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
-                Doorstop.Entrypoint.info.WriteLine("Finished Patching!");
-            } catch (Exception ex) {
-                Doorstop.Entrypoint.error.WriteLine(ex.ToString());
-            }
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
