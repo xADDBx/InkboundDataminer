0af788214d2087bc0ec8eff1f8e037063096ebae
ADDB <addb1234567890@gmail.com>
2023-09-30 22:15:06 +0200

    works

--- InkboundDataminer/Patches.cs
+++ InkboundDataminer/Patches.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using Mono.Btls;
+using Mono.Net.Security;
 using Mono.Security.Interface;
 using Mono.Unity;
 using NativeWebSocket;
@@ -28,75 +30,55 @@ namespace InkboundDataminer {
             }
         }
         */
-        [HarmonyPatch(typeof(UnityTlsContext))]
-        public static class UnityTlsContext_Patch {
-            [HarmonyPatch(nameof(UnityTlsContext.ProcessHandshake))]
+        [HarmonyPatch(typeof(Mono.Net.Security.MonoTlsProviderFactory))]
+        public static class MonoTlsProviderFactory_Patch {
+            [HarmonyPatch(nameof(Mono.Net.Security.MonoTlsProviderFactory.CreateDefaultProviderImpl))]
             [HarmonyPrefix]
-            public static bool ProcessHandshake(UnityTlsContext __instance, ref bool __result) {
-                Debugger.Break();
-                var file = new StreamWriter("MyLogWTF2.txt");
-                file.AutoFlush = true;
-                unsafe {
-                    __instance.lastException = null;
-                    UnityTls.unitytls_errorstate unitytls_errorstate = UnityTls.NativeInterface.unitytls_errorstate_create();
-                    UnityTls.unitytls_x509verify_result unitytls_x509verify_result = UnityTls.NativeInterface.unitytls_tlsctx_process_handshake(__instance.tlsContext, &unitytls_errorstate);
-                    /*if (unitytls_errorstate.code == UnityTls.unitytls_error_code.UNITYTLS_USER_WOULD_BLOCK) {
-                        __result = false;
-                        return false;
-                    }*/
-                    if (__instance.lastException != null) {
-                        throw __instance.lastException;
-                    }
-                    if (__instance.IsServer && unitytls_x509verify_result == (UnityTls.unitytls_x509verify_result)2147483648U) {
-                        Debug.CheckAndThrow(unitytls_errorstate, "Handshake failed", AlertDescription.HandshakeFailure);
-                        if (!__instance.ValidateCertificate(null, null)) {
-                            throw new TlsException(AlertDescription.HandshakeFailure, "Verification failure during handshake");
-                        }
-                    } else {
-                        Debug.CheckAndThrow(unitytls_errorstate, unitytls_x509verify_result, "Handshake failed", AlertDescription.HandshakeFailure);
-                    }
-                    file.Close();
-                    __result = true;
-                    return false;
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(WebSocket))]
-        public static class WebSocket_Patch {
-            [HarmonyPatch(nameof(WebSocket.Connect))]
-            [HarmonyPrefix]
-            public static bool Connect(WebSocket __instance) {
+            public static bool CreateDefaultProviderImpl(ref MobileTlsProvider __result) {
                 var file = new StreamWriter("MyLogWTF.txt");
                 file.AutoFlush = true;
+                file.WriteLine("Start!");
                 try {
-                    __instance.m_TokenSource = new CancellationTokenSource();
-                    __instance.m_CancellationToken = __instance.m_TokenSource.Token;
-                    __instance.m_Socket = new System.Net.WebSockets.ClientWebSocket();
-                    foreach (KeyValuePair<string, string> keyValuePair in __instance.headers) {
-                        __instance.m_Socket.Options.SetRequestHeader(keyValuePair.Key, keyValuePair.Value);
+                    string text = Environment.GetEnvironmentVariable("MONO_TLS_PROVIDER");
+                    if (string.IsNullOrEmpty(text)) {
+                        text = "default";
                     }
-                    foreach (string text in __instance.subprotocols) {
-                        __instance.m_Socket.Options.AddSubProtocol(text);
+                    if (!(text == "default") && !(text == "legacy")) {
+                        if (!(text == "btls")) {
+                            if (!(text == "unitytls")) {
+                                __result = Mono.Net.Security.MonoTlsProviderFactory.LookupProvider(text, true);
+                                file.WriteLine("Done");
+                                file.Close();
+                                return false;
+                            }
+                            goto IL_6E;
+                        }
+                    } else {
+                        if (UnityTls.IsSupported) {
+                            goto IL_6E;
+                        }
+                        if (!Mono.Net.Security.MonoTlsProviderFactory.IsBtlsSupported()) {
+                            throw new NotSupportedException("TLS Support not available.");
+                        }
                     }
-                    var task = __instance.m_Socket.ConnectAsync(__instance.uri, __instance.m_CancellationToken);
-                    task.Wait();
-                    file.WriteLine("What?");
-                    task = __instance.Receive();
-                    task.Wait();
+                    __result = new MonoBtlsProvider();
+                    file.WriteLine("Done");
+                    file.Close();
+                    return false;
+IL_6E:
+                    __result = new UnityTlsProvider();
+                    file.WriteLine("Done");
+                    file.Close();
+                    return false;
                 } catch (Exception ex) {
-                    file.WriteLine("Error");
                     file.WriteLine(ex.ToString());
-                } finally {
-                    if (__instance.m_Socket != null) {
-                        __instance.m_TokenSource.Cancel();
-                        __instance.m_Socket.Dispose();
-                    }
                 }
+                file.WriteLine("Done");
                 file.Close();
                 return false;
             }
         }
+
         public static void Patch() {
             try {
                 HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
